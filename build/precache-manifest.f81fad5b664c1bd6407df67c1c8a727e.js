17abbf99e17a72c0e30bcc64afdbc89a2c2d9f51
root <>
2022-05-31 21:47:05 +0300

    added build folder

---
+++ build/precache-manifest.f81fad5b664c1bd6407df67c1c8a727e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c4b9bad7c273b50e467",
+    "url": "/static/css/main.85e5e214.chunk.css"
+  },
+  {
+    "revision": "0c4b9bad7c273b50e467",
+    "url": "/static/js/main.0c4b9bad.chunk.js"
+  },
+  {
+    "revision": "8fd0f1333627161c58d8",
+    "url": "/static/js/1.8fd0f133.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69d03e4b5c951ea2058fa2af8a9d2db1",
+    "url": "/index.html"
+  }
+];
